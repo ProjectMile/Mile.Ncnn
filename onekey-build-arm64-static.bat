8a5527038662e771c0a9f715534b8090ee015884
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-04-11 21:50:59 +0800

    Add onekey-build-*-static.bat scripts.

---
+++ onekey-build-arm64-static.bat
@@ -0,0 +1,35 @@
+@setlocal
+
+@echo off
+
+set VisualStudioInstallerFolder="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer"
+if %PROCESSOR_ARCHITECTURE%==x86 set VisualStudioInstallerFolder="%ProgramFiles%\Microsoft Visual Studio\Installer"
+
+pushd %VisualStudioInstallerFolder%
+for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set VisualStudioInstallDir=%%i
+)
+popd
+
+call "%VisualStudioInstallDir%\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
+
+set ObjectFolder="%~dp0Output\Objects\arm64"
+set BinaryFolder="%~dp0Output\Binaries\arm64"
+
+rem Remove the output folder for a fresh compile.
+rd /s /q %ObjectFolder%
+rd /s /q %BinaryFolder%
+
+mkdir %BinaryFolder%
+
+mkdir %ObjectFolder%\ncnn
+pushd %ObjectFolder%\ncnn
+cmake -DCMAKE_INSTALL_PREFIX=%BinaryFolder% -G "Ninja" -DCMAKE_BUILD_TYPE=Release ../../../../ncnn
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+cmake --build . --parallel
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+ninja install
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+popd
+
+@endlocal
---
+++ onekey-build-x86-static.bat
@@ -0,0 +1,35 @@
+@setlocal
+
+@echo off
+
+set VisualStudioInstallerFolder="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer"
+if %PROCESSOR_ARCHITECTURE%==x86 set VisualStudioInstallerFolder="%ProgramFiles%\Microsoft Visual Studio\Installer"
+
+pushd %VisualStudioInstallerFolder%
+for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set VisualStudioInstallDir=%%i
+)
+popd
+
+call "%VisualStudioInstallDir%\VC\Auxiliary\Build\vcvarsall.bat" amd64_x86
+
+set ObjectFolder="%~dp0Output\Objects\x86"
+set BinaryFolder="%~dp0Output\Binaries\x86"
+
+rem Remove the output folder for a fresh compile.
+rd /s /q %ObjectFolder%
+rd /s /q %BinaryFolder%
+
+mkdir %BinaryFolder%
+
+mkdir %ObjectFolder%\ncnn
+pushd %ObjectFolder%\ncnn
+cmake -DCMAKE_INSTALL_PREFIX=%BinaryFolder% -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DNCNN_RUNTIME_CPU=OFF -DNCNN_AVX2=OFF -DNCNN_AVX=OFF -DNCNN_AVX512=OFF -DNCNN_XOP=OFF ../../../../ncnn
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+cmake --build . --parallel
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+ninja install
+if %ERRORLEVEL% NEQ 0 exit /B %ERRORLEVEL%
+popd
+
+@endlocal
